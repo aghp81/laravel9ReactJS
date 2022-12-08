7d9fbc497905ff5a480b49e0c10fbbc5f8a4d71e
abol ghana <aghp81@gmail.com>
2022-12-08 10:21:57 +0330

    inex.js - App.js make

---
+++ resources/js/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {/* Components Goes here */}
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+
+
 if (document.getElementById('employeeeApp')) {
     const Index = ReactDOM.createRoot(document.getElementById("employeeeApp"));
 
     Index.render(
         <React.StrictMode>
-            <Example/>
+            <App/>
         </React.StrictMode>
     )
 }
