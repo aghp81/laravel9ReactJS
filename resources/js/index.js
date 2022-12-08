66a3a96853b4932597e508b0e1002c0b5efb8df1
abol ghana <aghp81@gmail.com>
2022-12-08 10:13:23 +0330

    laravel9 ReactJS CRUD

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+if (document.getElementById('employeeeApp')) {
+    const Index = ReactDOM.createRoot(document.getElementById("employeeeApp"));
+
+    Index.render(
+        <React.StrictMode>
+            <Example/>
+        </React.StrictMode>
+    )
+}
