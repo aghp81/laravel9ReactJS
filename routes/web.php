ac280ce9287ac75f15702497002c602358dd6fe3
abol ghana <aghp81@gmail.com>
2022-12-09 19:37:24 +0330

    web.php - make Route for Employee List

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\EmployeesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,3 +18,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('get/employee/list', 
+    [EmployeesController::class, 'getEmployeeList'])->name('employee.list');
