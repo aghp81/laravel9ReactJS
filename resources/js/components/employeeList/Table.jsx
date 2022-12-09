522c2b27c255da198ca331977441578d8a48b046
abol ghana <aghp81@gmail.com>
2022-12-09 12:53:55 +0330

    make model migration factory seeder for Employee

--- resources/js/components/employeeList/Table.jsx
+++ resources/js/components/employeeList/Table.jsx
@@ -1,39 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Example = () => {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
+class Table  extends Component{
 
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                            <th scope="col" width="100px">#</th>
-                            <th scope="col" width="100px">Name</th>
-                            <th scope="col" width="100px">Salary</th>
-                            <th scope="col" width="100px">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            </tr>
-                        </tbody>
-                    </table>
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            employees: [],
+        }
+    }
+
+    //Get Employee list.
+    getEmployeeList = () => {
+
+    }
+
+
+    render(){
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+    
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col" width="100px">#</th>
+                                <th scope="col" width="100px">Name</th>
+                                <th scope="col" width="100px">Salary</th>
+                                <th scope="col" width="100px">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                            </tbody>
+                        </table>
+    
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default Example;
+
+
+export default Table;
 
 
