51b935c7d3d356df69486ad0c600ea1c21d54ec5
abol ghana <aghp81@gmail.com>
2022-12-10 17:22:26 +0330

    ViewModal.jsx - make modal with bootstrap

--- resources/js/components/employeeList/TableActionButtons.jsx
+++ resources/js/components/employeeList/TableActionButtons.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ViewModal from './Modals/ViewModal';
 
 
 class TableActionButtons  extends Component{
@@ -12,12 +13,16 @@ class TableActionButtons  extends Component{
         return(
             <div className="btn-group" role="group">
 
-                <button type="button" className="btn btn-primary">
+                
+                
+
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     View
                 </button>
+                <ViewModal />
 
 
-                
+
                 <button type="button" className="btn btn-info">Update</button>
                 <button type="button" className="btn btn-danger">Delete</button>
             </div>
