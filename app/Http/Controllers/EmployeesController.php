421bd8801794f434a5e029d7d9f24145aec1c24e
abol ghana <aghp81@gmail.com>
2022-12-15 22:59:52 +0330

    Storing new Employee

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -81,4 +81,31 @@ class EmployeesController extends Controller
         }
     }
 
+    // Storing new Employee.
+
+    public function store(Request $request)
+    {
+        try
+        {
+            $employeeName = $request->get('employeeName');
+            $employeeSalary = $request->get('employeeSalary');
+
+            Employee::create([
+                'employee_name'   => $employeeName,
+                'salary'          => $employeeSalary
+            ]);
+
+            return response()-> json([
+                'employee_name'   => $employeeName,
+                'salary' => $employeeSalary
+            ]);
+        }
+        catch(Exception $e)
+        {
+            Log::error($e);
+        }
+    }
+    
+    
+
 }
